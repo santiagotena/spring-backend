24d1db744faa486cc42724eb8bc22ef2732718db
Santiago Tena Hernandez <stenahe@gmail.com>
2023-10-08 16:57:10 +0200

    Add RestController methods

---
+++ src/main/java/com/stenahe/backend/repository/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.stenahe.backend.repository;
+
+import com.stenahe.backend.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    public Department findByDepartmentName(String departmentName);
+
+    public Department findByDepartmentNameIgnoreCase(String departmentName);
+}
---
+++ src/main/java/com/stenahe/backend/service/DepartmentService.java
@@ -0,0 +1,19 @@
+package com.stenahe.backend.service;
+
+import com.stenahe.backend.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
+}
